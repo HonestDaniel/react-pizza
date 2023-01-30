824dcced76efa52b4b322380b0a7b32501350b3e
Iesha Chromie <ieshachr@un-b2.kzn.21-school.ru>
2023-01-30 17:58:28 +0300

    before ts

--- src/App.js
+++ src/App.js
@@ -1,29 +1,25 @@
 import React from 'react';
-import Header from './components/Header'
 import Home from './pages/Home'
 import NotFound from './pages/NotFound';
 import Cart from './pages/Cart';
 import FullPizza from './pages/FullPizza';
 import {Routes, Route} from "react-router-dom"
 import './scss/app.scss'
-//import pizzas from './assets/pizzas.json'
+import MainLayout from './layouts/MainLayout';
 export const AppContext = React.createContext('');
 
 function App() {
 
   return (
-    <div className="wrapper">
-        <Header/>
-        <div className="content">
-            <Routes>
-              <Route path='/' element = {<Home/>}/>
-              <Route path='/react-pizza/' element = {<Home/>}/>
-              <Route path='/cart' element = {<Cart/>}/>
-              <Route path='/pizza/:id' element = {<FullPizza/>}/>
-              <Route path='*' element = {<NotFound/>}/>
-            </Routes>
-        </div>
-    </div>
+    <Routes>
+      <Route path='/' element = {<MainLayout />}>
+        <Route path='/' element = {<Home/>}/>
+        <Route path='/react-pizza/' element = {<Home/>}/>
+        <Route path='/cart' element = {<Cart/>}/>
+        <Route path='/pizza/:id' element = {<FullPizza/>}/>
+        <Route path='*' element = {<NotFound/>}/>
+      </Route>
+    </Routes>
   );
 }
 
